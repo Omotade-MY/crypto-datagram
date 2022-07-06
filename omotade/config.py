f0c687914dc4969f54e976769372c0b86c929ba6
Muhammad <muhammadyakub181@gmail.com>
2022-07-06 18:43:38 +0100

    cleaned branch

--- omotade/config.py
+++ omotade/config.py
@@ -6,15 +6,17 @@ Created on Tue May 17 22:32:11 2022
 """
 from sqlalchemy import create_engine
 
-#from auths import hostname, password
+from auths import hostname, password
 
 
-LOCAL_DATABASE_URI = "postgresql+psycopg2://postgres:udkhulbisalaam@localhost:5432/Cryptocurrency"
+#LOCAL_DATABASE_URI = "postgresql+psycopg2://postgres:udkhulbisalaam@localhost:5432/Cryptocurrency"
 
-#CLOUD_DATABASE_URL = "postgresql+psycopg2://postgres:"+password+"@"+hostname+":5432/Cryptocurrency"
+CLOUD_DATABASE_URI_TB = "postgresql+psycopg2://omotade:"+password+"@"+hostname+":5432/CryptoTransactDB"
+CLOUD_DATABASE_URI_PB = "postgresql+psycopg2://omotade:"+password+"@"+hostname+":5432/ProductionDB"
 
-local_engine = create_engine(LOCAL_DATABASE_URI)
-#cloud_engine = create_engine(CLOUD_DATABASE_URL)
+#local_engine = create_engine(LOCAL_DATABASE_URI)
+cloudtb_engine = create_engine(CLOUD_DATABASE_URI_TB)
+cloudpb_engine = create_engine(CLOUD_DATABASE_URI_PB)
 
 
     
